d5eeb35d7245dfb6c36fe29e1f9b18bd9602b833
NameAcht <appeljohannes21@gmail.com>
2024-01-25 23:01:25 +0100

    fix camera vars

--- opengl-playground/include/camera.h
+++ opengl-playground/include/camera.h
@@ -1,2 +1,10 @@
 #pragma once
 #include <cglm/struct.h>
+
+static vec3s cameraPos = {
+	0.0f, 0.0f, 3.0f
+};
+static vec3s cameraFront = {
+	0.0f, 0.0f, -1.0f
+};
+static float cameraSpeed = 0.1f;
--- opengl-playground/src/main.c
+++ opengl-playground/src/main.c
@@ -8,12 +8,12 @@
 #include <gl_test.h>
 
 int main(int argc, char** argv) {
-	//render_image_mix();
-	//spin_and_scale_2d();
-	//render_rectangle_3d();
-	//spin_cube();
-	//spin_10_cubes();
-	//rotate_scene();
+	render_image_mix();
+	spin_and_scale_2d();
+	render_rectangle_3d();
+	spin_cube();
+	spin_10_cubes();
+	rotate_scene();
 	free_movement();
 
 	return 0;
